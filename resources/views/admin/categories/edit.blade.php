63dd90414cd2954b5d39f9377502d98259f48aad
vaishali-agr13 <vaishali.agr13@gmail.com>
2026-05-03 17:00:11 +0530

    edit-category

--- resources/views/admin/categories/edit.blade.php
+++ resources/views/admin/categories/edit.blade.php
@@ -42,13 +42,102 @@
 
                <div class="form-group">
                 <label>Category Icon</label>
-                <input 
-                    type="text" 
-                    name="icon" 
-                    class="form-control" 
-                    value="{{ $category->icon }}"
-                    required
-                >
+                 <select name="icon" id="category" class="form-control" required>
+                        <option value="">-- Select Icon --</option>
+
+                        <option value="fa-laptop-code" {{ isset($category) && $category->icon == 'fa-laptop-code' ? 'selected' : '' }}>
+                            fa-laptop-code (IT / Software)
+                        </option>
+
+                        <option value="fa-code" {{ isset($category) && $category->icon == 'fa-code' ? 'selected' : '' }}>
+                            fa-code (Developer)
+                        </option>
+
+                        <option value="fa-palette" {{ isset($category) && $category->icon == 'fa-palette' ? 'selected' : '' }}>
+                            fa-palette (Web Designer)
+                        </option>
+
+                        <option value="fa-chart-line" {{ isset($category) && $category->icon == 'fa-chart-line' ? 'selected' : '' }}>
+                            fa-chart-line (Data Analyst)
+                        </option>
+
+                        <option value="fa-bullhorn" {{ isset($category) && $category->icon == 'fa-bullhorn' ? 'selected' : '' }}>
+                            fa-bullhorn (Marketing)
+                        </option>
+
+                        <option value="fa-sack-dollar" {{ isset($category) && $category->icon == 'fa-sack-dollar' ? 'selected' : '' }}>
+                            fa-sack-dollar (Sales)
+                        </option>
+
+                        <option value="fa-users" {{ isset($category) && $category->icon == 'fa-users' ? 'selected' : '' }}>
+                            fa-users (HR)
+                        </option>
+
+                        <option value="fa-building-columns" {{ isset($category) && $category->icon == 'fa-building-columns' ? 'selected' : '' }}>
+                            fa-building-columns (Finance)
+                        </option>
+
+                        <option value="fa-file-invoice-dollar" {{ isset($category) && $category->icon == 'fa-file-invoice-dollar' ? 'selected' : '' }}>
+                            fa-file-invoice-dollar (Accounting)
+                        </option>
+
+                        <option value="fa-headset" {{ isset($category) && $category->icon == 'fa-headset' ? 'selected' : '' }}>
+                            fa-headset (Customer Support)
+                        </option>
+
+                        <option value="fa-pen-nib" {{ isset($category) && $category->icon == 'fa-pen-nib' ? 'selected' : '' }}>
+                            fa-pen-nib (Content Writer)
+                        </option>
+
+                        <option value="fa-graduation-cap" {{ isset($category) && $category->icon == 'fa-graduation-cap' ? 'selected' : '' }}>
+                            fa-graduation-cap (Education)
+                        </option>
+
+                        <option value="fa-hospital" {{ isset($category) && $category->icon == 'fa-hospital' ? 'selected' : '' }}>
+                            fa-hospital (Healthcare)
+                        </option>
+
+                        <option value="fa-user-nurse" {{ isset($category) && $category->icon == 'fa-user-nurse' ? 'selected' : '' }}>
+                            fa-user-nurse (Nurse)
+                        </option>
+
+                        <option value="fa-user-doctor" {{ isset($category) && $category->icon == 'fa-user-doctor' ? 'selected' : '' }}>
+                            fa-user-doctor (Doctor)
+                        </option>
+
+                        <option value="fa-gears" {{ isset($category) && $category->icon == 'fa-gears' ? 'selected' : '' }}>
+                            fa-gears (Engineering)
+                        </option>
+
+                        <option value="fa-helmet-safety" {{ isset($category) && $category->icon == 'fa-helmet-safety' ? 'selected' : '' }}>
+                            fa-helmet-safety (Construction)
+                        </option>
+
+                        <option value="fa-truck" {{ isset($category) && $category->icon == 'fa-truck' ? 'selected' : '' }}>
+                            fa-truck (Logistics)
+                        </option>
+
+                        <option value="fa-shield-halved" {{ isset($category) && $category->icon == 'fa-shield-halved' ? 'selected' : '' }}>
+                            fa-shield-halved (Security)
+                        </option>
+
+                        <option value="fa-scale-balanced" {{ isset($category) && $category->icon == 'fa-scale-balanced' ? 'selected' : '' }}>
+                            fa-scale-balanced (Legal)
+                        </option>
+
+                        <option value="fa-briefcase" {{ isset($category) && $category->icon == 'fa-briefcase' ? 'selected' : '' }}>
+                            fa-briefcase (Admin / Office)
+                        </option>
+
+                        <option value="fa-globe" {{ isset($category) && $category->icon == 'fa-globe' ? 'selected' : '' }}>
+                            fa-globe (Freelance)
+                        </option>
+
+                        <option value="fa-house-laptop" {{ isset($category) && $category->icon == 'fa-house-laptop' ? 'selected' : '' }}>
+                            fa-house-laptop (Remote Jobs)
+                        </option>
+
+                    </select>
                 @error('name')
                     <small class="text-danger">{{ $message }}</small>
                 @enderror
